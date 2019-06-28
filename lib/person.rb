3c4263b29caeb38aaf58d219e31bb6983420746e
Amy Westcott <amy.westcott@yahoo.com>
2019-06-28 16:48:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,7 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight 
   
-  def initialize(name:. age:, location:)
-    @name = name 
-    @age = age 
-    @location = location
-  end 
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
